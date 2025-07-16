ec7ef0d4426bfbf887ed80f21e45d44122bb32b9
warfather0991 <nakib1990saleh@gmail.com>
2025-07-16 21:48:06 +0600

    second push

--- backend/src/main/java/com/eventlagbe/backend/controller/Hello.java
+++ backend/src/main/java/com/eventlagbe/backend/controller/Hello.java
@@ -1,4 +1,4 @@
-package com.backend.backend;
+package com.eventlagbe.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @RequestMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Hello World my name is Event Lagbe";
     }
 }
